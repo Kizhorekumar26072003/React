46d046b732eedb457ebc2029b0f13b92b2d709e9
Kizhore Kumar S S Corp(GET-IT PROJECTS) <M08503@corppdc.com>
2025-08-26 12:56:47 +0530

    useState Practice

--- useState/useState/src/App.jsx
+++ useState/useState/src/App.jsx
@@ -4,21 +4,37 @@ import React from 'react'
 //it return an array [variable , function]
 //we update the first value in the useState hook array using the second value function. our whole components will be re rendered
 const App = () => {
-  let h
-  let [num,setNum] = useState(1);
-  const add = ()=>{
-    
-    setNum((val)=>{
-      return val + 1
-    })
-                       
-  }
-  return <>
 
-  <h1>{num}</h1>
-  <button onClick={add}>⚡</button>
+  // COUNTER
+  // let h
+  // let [num,setNum] = useState(1);
+  // const add = ()=>{
+    
+  //   setNum((val)=>{
+  //     return val + 1
+  //   })
+                       
+  // }
+  // return <>
+
+  // <h1>{num}</h1>
+  // <button onClick={add}>⚡</button>
   
+  // </>
+
+  const [ip, setIp] = useState("");
+
+   const changing = ()=>{
+    const val = document.querySelector("input").value;
+      setIp(val);
+      console.log(val);
+   }
+  return <>
+    <input type="text"/>
+    <button onClick={changing}>click</button>
+    <h1>{ip}</h1>
   </>
+
 }
 
 export default App
